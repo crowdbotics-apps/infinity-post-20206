b48480db2fdd82c6b1d071ad2bc5a651f1e4e87a
Crowdbotics <team@crowdbotics.com>
2020-09-11 15:31:31 +0000

    Initial commit.

---
+++ ios/infinity_post_20206Tests/infinity_post_20206Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface infinity_post_20206 : XCTestCase
+
+@end
+
+@implementation infinity_post_20206
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
